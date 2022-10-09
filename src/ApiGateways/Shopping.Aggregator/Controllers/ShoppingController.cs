33b50be60552cb60e3dd78239f71a1e6540bb3e7
Samuel Iyinoluwa Karonwi <iyinoluwasamuel@gmail.com>
2022-10-09 16:54:40 +0100

    Razor pages included

---
+++ src/ApiGateways/Shopping.Aggregator/Controllers/ShoppingController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shopping.Aggregator.Models;
+using Shopping.Aggregator.Services;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Shopping.Aggregator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShoppingController : ControllerBase
+    {
+        private readonly IBasketService _basketService;
+        private readonly IOrderService _orderService;
+        private readonly ICatalogService _catalogService;
+
+        public ShoppingController(IBasketService basketService, IOrderService orderService, ICatalogService catalogService)
+        {
+            _basketService = basketService;
+            _orderService = orderService;
+            _catalogService = catalogService;
+        }
+
+        [HttpGet("{userName}", Name ="GetShoppping")]
+        [ProducesResponseType(typeof(ShoppingModel),(int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingModel>> GetShopping(string userName)
+        {
+            var basket = await _basketService.GetBasket(userName);
+            foreach (var item in basket.Items)
+            {
+                var product = await _catalogService.GetCatalog(item.ProductId);
+
+                item.ProductName = product.Name;
+                item.Category = product.Category;
+                item.Summary = product.Summary;
+                item.Description = product.Description;
+                item.ImageFile = product.ImageFile;
+            }
+
+            var orders = await _orderService.GetOrdersByUserName(userName);
+
+            var shopppingModel = new ShoppingModel
+            {
+                UserName = userName,
+                BasketWithProducts = basket,
+                Orders = orders
+            };
+
+            return Ok(shopppingModel);
+        } 
+    }
+}
